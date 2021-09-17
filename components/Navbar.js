d124bb566043c9ca1543e308dbc4b8282dba82ca
jhonnierandrey <jhonnierandrey@outlook.com>
2021-09-16 23:51:33 -0300

    fixing issues with functional components

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,5 +1,5 @@
-import Link from 'next/Link';
-import Image from 'next/Image';
+import Link from 'next/link';
+import Image from 'next/image';
 import logo from '../public/img/disney.png';
 
 const Navbar = ({account}) => {
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-import Link from 'next/Link';
-import Image from 'next/Image';
+import Link from 'next/link';
+import Image from 'next/image';
 import { gql, GraphQLClient } from 'graphql-request';
 import Section from '../components/Section';
 import Navbar from '../components/Navbar';
