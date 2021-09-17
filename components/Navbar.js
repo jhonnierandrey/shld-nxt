dd51d7986e23020b0331d697bb3a7985d62efa12
jhonnierandrey <jhonnierandrey@outlook.com>
2021-09-17 00:57:23 -0300

    adding menu with icons

--- components/Navbar.js
+++ components/Navbar.js
@@ -6,10 +6,30 @@ const Navbar = ({account}) => {
     return (
         <nav className="navbar">
             <div className="logo-wrapper">
-                <Link href="/">
+                <Link href="/" passHref>
                     <>
                         <Image src={logo} alt="Logo" width={90} height={50} />
                     </>
+                </Link>
+            </div>
+            <div className="nav-menu">
+                <Link href="/" passHref>
+                    <span className="nav-menu-item"><i className="bi bi-house-door-fill"></i> HOME</span>
+                </Link>
+                <Link href="/" passHref>
+                    <span className="nav-menu-item"><i className="bi bi-search"></i> SEARCH</span>
+                </Link>
+                <Link href="/" passHref>
+                    <span className="nav-menu-item"><i className="bi bi-plus-lg"></i> WATCHLIST</span>
+                </Link>
+                <Link href="/" passHref>
+                    <span className="nav-menu-item"><i className="bi bi-star-fill"></i> ORIGINALS</span>
+                </Link>
+                <Link href="/" passHref>
+                    <span className="nav-menu-item"><i className="bi bi-film"></i> MOVIES</span>
+                </Link>
+                <Link href="/" passHref>
+                    <span className="nav-menu-item"><i className="bi bi-tv-fill"></i> SERIES</span>
                 </Link>
             </div>
             <div className="account-info">
